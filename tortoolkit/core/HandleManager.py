ae466f833a3fe37f233a20f87176f63a0cb28326
aniketgohel-dev <72062359+aniketgohel-dev@users.noreply.github.com>
2021-08-17 10:48:24 +0530

    Update HandleManager.py

--- tortoolkit/core/HandleManager.py
+++ tortoolkit/core/HandleManager.py
@@ -643,7 +643,7 @@ async def set_password_zip(message):
 
 
 async def start_handler(event):
-    msg = "Hello This is TorToolkitX running on heroku an instance of <a href='https://github.com/XcodersHub/TorToolkitX'>This Repo</a>. Try the repo for yourself and dont forget to put a STAR and fork."
+    msg = "Hello I'm torrent/link to tg bot for use try leech or else contact @anikket123"
     await event.reply(msg, parse_mode="html")
 
 
